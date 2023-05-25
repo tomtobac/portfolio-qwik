eaa01f638bbbd12648d684f583255c7c74787bcd
ArturoZarzalejo <arturoazh@gmail.com>
2023-05-25 17:08:05 +0200

    youtube

--- src/routes/portfolio/index.tsx
+++ src/routes/portfolio/index.tsx
@@ -1,14 +1,15 @@
-import { component$, useVisibleTask$, useTask$, useStore, useStylesScoped$, unwrapProxy } from '@builder.io/qwik';
+import { component$, useVisibleTask$, useTask$, useStore, useStylesScoped$ } from '@builder.io/qwik';
 import { type DocumentHead } from '@builder.io/qwik-city';
 import styles from './portfolio.css?inline';
+import { google } from 'googleapis';
 
-const apiUrlServer = 'http://localhost:5173/server/api-github-mock.json';
+console.log(google);
+
+
 
 export default component$(() => {
 
   useStylesScoped$(styles);
-
-  // const data = { data: {} }
 
   const state = useStore({
     apiUrl: 'https://api.github.com/users/arturozarzalejo',
@@ -18,16 +19,41 @@ export default component$(() => {
   });
 
   useTask$(async () => {
-    state.dataServerJson = await fetch(apiUrlServer)
+    state.dataServerJson = await fetch(state.apiUrlServer)
       .then(response => response.json())
       .then(data => data)
       .catch(error => {
         // Manejo de errores
         console.log('Error:', error);
       });
+
   });
 
   useVisibleTask$(async ({ cleanup }) => {
+
+    console.log('google', google)
+
+    // const apiKey = 'AIzaSyDjyp6v1Zb1SV8JdeoV-rLT_rR1MONAL9U';
+    // const youtube = google.youtube({
+    //   version: 'v3',
+    //   auth: apiKey
+    // });
+
+    // youtube.search.list({
+    //   part: 'snippet',
+    //   channelId: 'ID_DEL_CANAL',
+    //   order: 'date',
+    //   type: 'video',
+    //   videoDuration: 'short',
+    //   maxResults: 10
+    // })
+    //   .then((response) => {
+    //     const shorts = response.data.items;
+    //     // Procesa la respuesta de la API
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error al realizar la solicitud:', error);
+    //   });
 
     console.log('cositas?2', state.dataServerJson)
 
