ae1b6091c8fe126bc83ce37f00d8d1eb66d63231
ArturoZarzalejo <arturoazh@gmail.com>
2023-06-01 12:07:08 +0200

    last changes

--- src/routes/portfolio/index.tsx
+++ src/routes/portfolio/index.tsx
@@ -1,63 +1,79 @@
-import { component$, useVisibleTask$, useTask$, useStore, useStylesScoped$ } from '@builder.io/qwik';
-import { type DocumentHead } from '@builder.io/qwik-city';
-import styles from './portfolio.css?inline';
-import { google } from 'googleapis';
+import {
+  component$,
+  useVisibleTask$,
+  useTask$,
+  useStore,
+  useStylesScoped$,
+} from "@builder.io/qwik";
+import { type DocumentHead } from "@builder.io/qwik-city";
+import styles from "./portfolio.css?inline";
 
-console.log(google);
+async function obtenerShortsYVideosDeCanal(apiKey, channelId) {
+  try {
+    const url = `https://www.googleapis.com/youtube/v3/search?channelId=${channelId}&maxResults=10&channelType=any&part=snippet&key=${apiKey}`
+    // https://content-youtube.googleapis.com/youtube/v3/search?channelId=UC9h5heKFR7KaoLSzjWIIxjw&channelType=any&part=snippet&maxResults=10&key=AIzaSyAa8yy0GdcGPHdtD083HiGGx_S0vMPScDM
 
+    const response = await fetch(url);
 
+    if (!response.ok) {
+      throw new Error("Error al obtener los shorts y videos");
+    }
+
+    const data = await response.json();
+
+    const shorts = data.items;
+    console.log("Shorts:", shorts);
+
+    // Obtén los videos del canal (realiza otra solicitud si es necesario)
+    // ...
+  } catch (error) {
+    console.error("Error al obtener los shorts y videos:", error);
+  }
+}
 
 export default component$(() => {
-
   useStylesScoped$(styles);
 
   const state = useStore({
-    apiUrl: 'https://api.github.com/users/arturozarzalejo',
-    apiUrlLocal: '/api-github-mock.json',
-    apiUrlServer: 'http://localhost:5173/server/api-github-mock.json',
-    dataServerJson: {}
+    apiUrl: "https://api.github.com/users/arturozarzalejo",
+    apiUrlLocal: "/api-github-mock.json",
+    apiUrlServer: "http://localhost:5173/server/api-github-mock.json",
+    dataServerJson: {},
   });
 
   useTask$(async () => {
     state.dataServerJson = await fetch(state.apiUrlServer)
-      .then(response => response.json())
-      .then(data => data)
-      .catch(error => {
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((error) => {
         // Manejo de errores
-        console.log('Error:', error);
+        console.log("Error:", error);
       });
-
   });
 
   useVisibleTask$(async ({ cleanup }) => {
+    const apiKey = "AIzaSyDjyp6v1Zb1SV8JdeoV-rLT_rR1MONAL9U";
+    const channelId = "UC9h5heKFR7KaoLSzjWIIxjw"; // ID del canal "arturodevelop"
 
-    console.log('google', google)
+    const youtubeVideos = obtenerShortsYVideosDeCanal(apiKey, channelId);
 
-    // const apiKey = 'AIzaSyDjyp6v1Zb1SV8JdeoV-rLT_rR1MONAL9U';
-    // const youtube = google.youtube({
-    //   version: 'v3',
-    //   auth: apiKey
-    // });
+    console.log("videosyoutube", youtubeVideos);
 
-    // youtube.search.list({
-    //   part: 'snippet',
-    //   channelId: 'ID_DEL_CANAL',
-    //   order: 'date',
-    //   type: 'video',
-    //   videoDuration: 'short',
-    //   maxResults: 10
-    // })
-    //   .then((response) => {
-    //     const shorts = response.data.items;
-    //     // Procesa la respuesta de la API
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error al realizar la solicitud:', error);
-    //   });
+    const apiUrl = `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${apiKey}`;
 
-    console.log('cositas?2', state.dataServerJson)
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        // Maneja la respuesta del API
+        console.log(data);
+      })
+      .catch((error) => {
+        // Maneja cualquier error de la llamada al API
+        console.error(error);
+      });
 
-  })
+    console.log("cositas?2", state.dataServerJson);
+  });
 
   return (
     <div class="container container-center">
@@ -66,12 +82,10 @@ export default component$(() => {
         <span class="highlight">Portfolio {state.dataServerJson.name}</span>
         <img class="img-rounded" src={state.dataServerJson.avatar_url} alt="" />
       </h1>
-
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Qwik Flower',
+  title: "Qwik Flower",
 };
-
