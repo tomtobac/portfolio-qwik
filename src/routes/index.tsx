1a00a5c6339809ed22bdf348c847fd71c5566313
arturozarzalejo <arturoazh@gmail.com>
2023-06-04 19:36:04 +0200

    cositas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,45 +3,51 @@ import {
   useVisibleTask$,
   useTask$,
   useStore,
-  noSerialize,
   useStylesScoped$,
   $,
 } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
 import { Twitch } from "../utils/api-twitch";
+import MarkdownIt from 'markdown-it';
 
 import styles from "./styles.css?inline";
+import mdStyles from "../utils/md-css.css?inline";
 
-const twitchDataInfo = $(async function (name: string, state: any) {
+const twitchDataInfo = $(async function (name: string, state: any, apiTwitch: any) {
   const channelName = name || "ArturoZarzalejo";
 
-  // console.log(state.twitch.api);
-
-  state.twitch.userInfo = await state.twitch.api.getUser(
+  state.twitch.userInfo = await apiTwitch.getUser(
     channelName,
     state.twitch.token.access_token,
     true
   );
 
-  state.twitch.channelInfo = await state.twitch.api.getChannel(
+  state.twitch.channelInfo = await apiTwitch.getChannel(
     state.twitch.userInfo.data[0].id,
     state.twitch.token.access_token,
     true
   );
 
-  state.twitch.categoryInfo = await state.twitch.api.getCategory(
+  state.twitch.categoryInfo = await apiTwitch.getCategory(
     state.twitch.channelInfo.data[0].game_id,
     state.twitch.token.access_token,
     true
   );
+
+  state.twitch.streams = await apiTwitch.getStreams(
+    state.twitch.userInfo.data[0].id,
+    state.twitch.token.access_token,
+    true
+  );
+
 });
 
-async function obtenerShortsYVideosDeCanal(apiKey, channelId) {
+async function obtenerShortsYVideosDeCanal(apiKey: string, channelId: string, state: any) {
   try {
     const url = `https://www.googleapis.com/youtube/v3/search?channelId=${channelId}&maxResults=10&channelType=any&type=video&order=date&part=snippet&key=${apiKey}`;
     // https://content-youtube.googleapis.com/youtube/v3/search?channelId=UC9h5heKFR7KaoLSzjWIIxjw&channelType=any&part=snippet&maxResults=10&key=AIzaSyAa8yy0GdcGPHdtD083HiGGx_S0vMPScDM
 
-    const response = await fetch(url);
+    const response = await fetch(state.apiYoutubeMock);
 
     if (!response.ok) {
       throw new Error("Error al obtener los shorts y videos");
@@ -61,29 +67,47 @@ async function obtenerShortsYVideosDeCanal(apiKey, channelId) {
 }
 
 export default component$(() => {
+  useStylesScoped$(mdStyles);
   useStylesScoped$(styles);
+  
 
   const state = useStore<any>({
     apiUrl: "https://api.github.com/users/arturozarzalejo",
     apiUrlLocal: "/api-github-mock.json",
-    apiUrlServer: "http://localhost:5173/server/api-github-mock.json",
+    apiGithubMock: "http://localhost:5173/server/api-github-mock.json",
+    apiYoutubeMock: "http://localhost:5173/server/api-youtube-mock.json",
     dataServerJson: {},
     github: {
       repos: [],
     },
     twitch: {
-      api: noSerialize({}),
       token: {},
       userInfo: {},
       categoryInfo: {},
       channelInfo: {},
+      streams: {}
     },
     youtube: {
       videos: [],
     },
   });
 
-  const pruebaClick = $(async () => {
+  const getMarkdown = $(async (repoURL: string) => {
+
+    const markdown = new MarkdownIt({
+      encoding: 'utf-8'
+    });
+    const readme = await fetch(`${repoURL}/readme`)
+      .then(data => data.json())
+      .then(data => data);
+
+    state.github.readme = markdown.render(atob(readme.content));
+
+    console.log('lo que queremos', readme)
+  })
+
+  const getRepos = $(async () => {
+
     const repos = await fetch(
       `https://api.github.com/users/ArturoZarzalejo/repos`
     )
@@ -93,6 +117,7 @@ export default component$(() => {
         // Manejo de errores
         console.log("Error:", error);
       });
+
     state.github.repos = repos.sort((a, b) =>
       b.pushed_at.localeCompare(a.pushed_at)
     );
@@ -101,7 +126,7 @@ export default component$(() => {
   });
 
   useTask$(async () => {
-    state.dataServerJson = await fetch(state.apiUrlServer)
+    state.dataServerJson = await fetch(state.apiGithubMock)
       .then((response) => response.json())
       .then((data) => data)
       .catch((error) => {
@@ -114,23 +139,35 @@ export default component$(() => {
     const apiKey = "AIzaSyDjyp6v1Zb1SV8JdeoV-rLT_rR1MONAL9U";
     const channelId = "UC9h5heKFR7KaoLSzjWIIxjw"; // ID del canal "arturodevelop"
 
-    state.youtube.videos = await obtenerShortsYVideosDeCanal(apiKey, channelId);
+    state.youtube.videos = await obtenerShortsYVideosDeCanal(apiKey, channelId, state);
 
-    state.twitch.api = noSerialize(new Twitch());
-    state.twitch.token = await state.twitch.api.getToken();
-    await twitchDataInfo("ArturoDevelop", state);
+    const apiTwitch = new Twitch()
+    state.twitch.token = await apiTwitch.getToken();
+    await twitchDataInfo("ArturoDevelop", state, apiTwitch);
 
     console.log("cositas?", state);
   });
 
   return (
     <div class="container container-center">
-      <img class="img-rounded" src={state.dataServerJson.avatar_url} alt="" />
+      <img class={`img-rounded ${state.twitch.streams.data && state.twitch.streams.data[0].type === 'live' && `live-image`}`} src={state.dataServerJson.avatar_url} alt="" />
 
-      <div role="presentation" class="ellipsis"></div>
+      {/* <div role="presentation" class="ellipsis"></div> */}
       <h1>
         <span class="highlight">{state.dataServerJson.name}</span>
       </h1>
+      <div class="card card-background-color card-box-shadow card-border-radius">
+        <div class="md-css" dangerouslySetInnerHTML={state.github.readme}></div>
+      </div>
+      <p>Muy buenas!! Bienvenido a parte de mi vida, probablemente, ahora mismo esté haciendo stream,
+        si no es así, me pillarás trabajando, comiendo como un gordito, o en el gimnasio para poder comer más.
+        Aquí te dejo mis diferentes proyectos y información sobre mi, podrás ver mis videos de youtube o ver mis proyectos en Github </p>
+      <div>
+        <div class="card card-background-color card-box-shadow card-border-radius">
+          {state.twitch.streams.data && <img class="thumbnail-twitch card-border-radius" src={state.twitch.streams.data[0].thumbnail_url.replace('{width}x{height}', '1920x1080')} alt="" /> || ``}
+        </div>
+
+      </div>
 
       <div class="icons-socials">
         <svg
@@ -148,7 +185,7 @@ export default component$(() => {
           />
         </svg>
         <svg
-          onClick$={() => pruebaClick()}
+          onClick$={() => getRepos()}
           xmlns="http://www.w3.org/2000/svg"
           width="317"
           height="317"
@@ -186,6 +223,7 @@ export default component$(() => {
                 src={video.snippet.thumbnails.high.url}
                 alt=""
               />
+              <span>{video.snippet.title}</span>
             </li>
           ))}
       </ul>
@@ -194,13 +232,7 @@ export default component$(() => {
           state.github.repos.map((repo) => (
             <li
               key={repo.id}
-              onClick$={() =>
-                fetch(`${repo.url}/readme`)
-                  .then((data) => data.json())
-                  .then((data) => {
-                    console.log(data, atob(data.content));
-                  })
-              }
+              onClick$={() => getMarkdown(repo.url)}
             >
               {repo.name}
             </li>
